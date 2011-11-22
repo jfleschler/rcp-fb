dedfd08930bbc75b5906a86f687ca3fdeebe5fa4
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-22 09:46:03 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RcpFb::Application.config.secret_token = '38fdfa9158339dfec3430036f2662f182e536d1e9580973204999b12d16c23ca026c39e55a0b3878f6ecd6bd5a67a68f93d1d3084ddd4e72fe41dfd837c345a2'
