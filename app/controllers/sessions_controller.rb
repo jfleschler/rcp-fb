1b39cc97dc4b6c149bce23e4ab7801ecd0c6fbb1
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 11:48:46 -0500

    fb sdk

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
   	user = User.where(:provider => auth['provider'], 
-                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
+                      :uid => auth['uid']).first || User.create_with_omniauth(auth)
   	session[:user_id] = user.id
   	redirect_back_or root_url
   end
 
   def destroy
-	reset_session
-	redirect_to root_url, :notice => 'Signed out!'
+	  reset_session
+	  redirect_to root_url, :notice => 'Signed out!'
   end
 
   def new
