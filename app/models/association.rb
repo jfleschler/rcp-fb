ab26ef3f6022b75eff34c26d6b0ebc84b67d71cd
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 15:54:52 -0500

    test

--- app/models/association.rb
+++ app/models/association.rb
@@ -8,8 +8,8 @@ class Association
 
   before_destroy :renumber_remaining
 
-  scope :ordered, lambda { order(:position) }
-	scope :list, lambda { joins(:ingredient).where("ingredients.recipe_id is null").order(:ingredient_id) }
+scope :ordered, lambda { order_by([:position, :asc]) }
+	scope :list, lambda { joins(:ingredient).where("ingredients.recipe_id is null").order_by([:ingredient_id, :asc]) }
 
   def ingredient
   	Ingredient.find(ingredient_id)
