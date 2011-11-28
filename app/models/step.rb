190c2b96296eb03506c70740541dd5f22c95a7b5
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 13:58:56 -0500

    fixed number_string

--- app/models/step.rb
+++ app/models/step.rb
@@ -12,25 +12,44 @@ class Association
 
 	def number_string
 		case step_num
-			when 1 "q"
-			when 2 "w"
-			when 3 "e"
-			when 4 "r"
-			when 5 "t"
-			when 6 "y"
-			when 7 "u"
-			when 8 "i"
-			when 9 "o"
-			when 10	"a"
-			when 11	"s"
-			when 12	"d"
-			when 13	"f"
-			when 14	"g"
-			when 15	"h"
-			when 16	"j"
-			when 17	"k"
-			when 18	"l"
-			when 19	";"
+			when 1 
+				"q"
+			when 2 
+				"w"
+			when 3 
+				"e"
+			when 4 
+				"r"
+			when 5 
+				"t"
+			when 6 
+				"y"
+			when 7 
+				"u"
+			when 8 
+				"i"
+			when 9 
+				"o"
+			when 10	
+				"a"
+			when 11	
+				"s"
+			when 12	
+				"d"
+			when 13	
+				"f"
+			when 14	
+				"g"
+			when 15	
+				"h"
+			when 16	
+				"j"
+			when 17	
+				"k"
+			when 18	
+				"l"
+			when 19	
+				";"
 		end
 	end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
 
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+
+ingredients = [
+	{:name => "salted butter",		:image => "ingredients/butter.png",		:tag_name => ""},
+	{:name => "unsalted butter",	:image => "ingredients/butter.png",		:tag_name => ""},
+	{:name => "white sugar",		:image => "placeholder.png",			:tag_name => ""},
+	{:name => "dark brown sugar",	:image => "placeholder.png",			:tag_name => ""},
+	{:name => "light brown sugar",	:image => "placeholder.png",			:tag_name => ""},
+	{:name => "egg",				:image => "ingredients/egg.png",		:tag_name => ""},
+	{:name => "egg white",			:image => "placeholder.png",			:tag_name => ""},
+	{:name => "vanilla extract",	:image => "ingredients/vanilla.png",	:tag_name => ""},
+	{:name => "all-purpose flour",	:image => "ingredients/flour.png",		:tag_name => ""},
+	{:name => "salt",				:image => "ingredients/salt.png",		:tag_name => ""},
+	{:name => "baking powder",		:image => "placeholder.png",			:tag_name => ""},
+	{:name => "baking soda",		:image => "placeholder.png",			:tag_name => ""},
+	{:name => "chocolate chips",	:image => "placeholder.png",			:tag_name => "chocolate"},
+	{:name => "cinnamon",			:image => "placeholder.png",			:tag_name => "cinnamon"},
+	{:name => "milk",				:image => "placeholder.png",			:tag_name => ""},
+	{:name => "raisins",			:image => "placeholder.png",			:tag_name => "raisins"},
+	{:name => "cake flour",			:image => "ingredients/flour.png",		:tag_name => ""},
+	{:name => "strawberries",		:image => "ingredients/strawberry.png",	:tag_name => ""}
+].each do |i|
+	ingredient = Ingredient.find_or_create_by_name(i)
+	ingredient.update_attributes(i)
+	ingredient.save
+end
