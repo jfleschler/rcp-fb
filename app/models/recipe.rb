142d0ace7179eff41c682c76864e683c1bdf3551
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 14:57:57 -0500

    test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,4 +9,36 @@ class Recipe
   field :cook_unit,   :type => String
 
   belongs_to :user
+
+  default_scope order(:id)
+	scope :public, lambda { where(:public => true) }
+
+	before_destroy :remove_ingredient
+
+	def step_attributes=(step_attributes)
+	  step_attributes.each do |attributes|
+	    steps.build(attributes)
+	  end
+	end
+
+	def temperature
+		"#{cook_temp.to_s}#{temp_unit.to_s}"
+	end
+
+	def remove_ingredient
+		unless has_ingredient == nil
+			i = Ingredient.find(has_ingredient)
+			i.destroy
+		end
+	end
+
+	def recipes_as_ingredients
+		r_as_i = []
+		associations.each do |a|
+			unless a.ingredient.recipe_id.nil?
+				r_as_i << Recipe.find_by_id(a.ingredient.recipe_id)
+			end
+		end
+		return r_as_i
+	end
 end
