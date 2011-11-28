9484fb1156c6b3f878502005efdad78b3e92f17e
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 13:10:37 -0500

    align image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def fb_login_btn
-		img = image_tag "facebook.png"
+		img = image_tag "facebook.png", :id => "facebook_image"
 		link_to img + " log in", signin_path
 	end
 end
