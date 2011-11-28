df2201aed8f63e2eef300834f8847ac48b897be8
Jon Fleschler <jon.fleschler@gmail.com>
2011-11-28 11:23:42 -0500

    current_user.name

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ RcpFb::Application.routes.draw do
   
   root :to => 'home#index'
 
-  match '/auth/:provider/callback' => 'sessions#create'
-  match '/auth/failure' => 'sessions#failure'
+  match '/auth/:provider/callback' 	=> 'sessions#create'
+  match '/auth/failure' 			=> 'sessions#failure'
 
   match '/signout' => 'sessions#destroy', :as => :signout
-  match '/signin' => 'sessions#new', :as => :signin
+  match '/signin'  => 'sessions#new', :as => :signin
 end
