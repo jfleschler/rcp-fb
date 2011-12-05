f24669947f94baca3913b7e235150f1f32055861
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-05 10:32:49 -0500

    reload patterns.scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 
 ingredients = [
-	{:name => "salted butter",		:image => "ingredients/butter.png",		:tag_name => ""},
-	{:name => "unsalted butter",	:image => "ingredients/butter.png",		:tag_name => ""},
-	{:name => "white sugar",		:image => "placeholder.png",			:tag_name => ""},
-	{:name => "dark brown sugar",	:image => "placeholder.png",			:tag_name => ""},
-	{:name => "light brown sugar",	:image => "placeholder.png",			:tag_name => ""},
-	{:name => "egg",				:image => "ingredients/egg.png",		:tag_name => ""},
-	{:name => "egg white",			:image => "placeholder.png",			:tag_name => ""},
-	{:name => "vanilla extract",	:image => "ingredients/vanilla.png",	:tag_name => ""},
-	{:name => "all-purpose flour",	:image => "ingredients/flour.png",		:tag_name => ""},
-	{:name => "salt",				:image => "ingredients/salt.png",		:tag_name => ""},
-	{:name => "baking powder",		:image => "placeholder.png",			:tag_name => ""},
-	{:name => "baking soda",		:image => "placeholder.png",			:tag_name => ""},
-	{:name => "chocolate chips",	:image => "placeholder.png",			:tag_name => "chocolate"},
-	{:name => "cinnamon",			:image => "placeholder.png",			:tag_name => "cinnamon"},
-	{:name => "milk",				:image => "placeholder.png",			:tag_name => ""},
-	{:name => "raisins",			:image => "placeholder.png",			:tag_name => "raisins"},
-	{:name => "cake flour",			:image => "ingredients/flour.png",		:tag_name => ""},
-	{:name => "strawberries",		:image => "ingredients/strawberry.png",	:tag_name => ""}
+	{:name => "salted butter",		:image => "ingredients/butter.png"},
+	{:name => "unsalted butter",	:image => "ingredients/butter.png"},
+	{:name => "white sugar",		:image => "placeholder.png"},
+	{:name => "dark brown sugar",	:image => "placeholder.png"},
+	{:name => "light brown sugar",	:image => "placeholder.png"},
+	{:name => "egg",				:image => "ingredients/egg.png"},
+	{:name => "egg white",			:image => "placeholder.png"},
+	{:name => "vanilla extract",	:image => "ingredients/vanilla.png"},
+	{:name => "all-purpose flour",	:image => "ingredients/flour.png"},
+	{:name => "salt",				:image => "ingredients/salt.png"},
+	{:name => "baking powder",		:image => "placeholder.png"},
+	{:name => "baking soda",		:image => "placeholder.png"},
+	{:name => "chocolate chips",	:image => "placeholder.png"},
+	{:name => "cinnamon",			:image => "placeholder.png"},
+	{:name => "milk",				:image => "placeholder.png"},
+	{:name => "raisins",			:image => "placeholder.png"},
+	{:name => "cake flour",			:image => "ingredients/flour.png"},
+	{:name => "strawberries",		:image => "ingredients/strawberry.png"}
 ].each do |i|
 	ingredient = Ingredient.find_or_create_by(i)
 	ingredient.update_attributes(i)
