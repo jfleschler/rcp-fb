b64e78c7a73f9ed1b2c25de6aae2ed09ad13ac47
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-09 17:04:51 -0500

    remove store dir

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,10 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [200, 200]
   end
 
-  def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   def cache_dir
 		"#{Rails.root}/tmp/uploads"
 	end
