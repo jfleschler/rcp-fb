3db8e97dfbc6e6d436add36193f511d826ec0ee2
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-09 16:52:58 -0500

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,14 @@ CarrierWave.configure do |config|
   config.grid_fs_database = Mongoid.database.name
   config.grid_fs_host = Mongoid.config.master.connection.host
   config.storage = :grid_fs
+
+
+  uri = URI.parse(ENV['MONGOLAB_URI'])
+  config.grid_fs_database = File.basename(uri.path)
+  config.grid_fs_host = uri.host unless uri.host.blank?
+  config.grid_fs_port = uri.port unless uri.port.blank?
+  config.grid_fs_username = uri.user unless uri.user.blank?
+  config.grid_fs_password = uri.password unless uri.password.blank?
+
   config.grid_fs_access_url = "/images"
 end
