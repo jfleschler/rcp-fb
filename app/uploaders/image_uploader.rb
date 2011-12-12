7a3263f2135e2afba9ccee0ab365aa4e0d997641
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-12 11:51:44 -0500

    round

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
 	include CarrierWave::RMagick
+  include CarrierWave::Round
   
   storage :grid_fs
 
@@ -16,19 +17,5 @@ class ImageUploader < CarrierWave::Uploader::Base
 		"#{Rails.root}/tmp/uploads"
 	end
 
-  def rounded_corner(radius = 25)
-    manipulate! do |img|
-        masq = ::Magick::Image.new(img.columns, img.rows).matte_floodfill(1, 1)
-            ::Magick::Draw.new.
-            fill('transparent').
-            stroke('black').
-            stroke_width(1).
-            roundrectangle(0, 0, img.columns - 1, img.rows - 1, radius, radius).
-            draw(masq)
-
-        img.composite!(masq, 0, 0, Magick::LightenCompositeOp)
-        img = yield(img) if block_given?
-        img
-    end
-  end
+  
 end
