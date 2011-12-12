42cc1f425e4fe0dca5f727cfa018eb5761e77bd6
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-12 11:09:10 -0500

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,10 @@ CarrierWave.configure do |config|
   #config.grid_fs_connection = ENV['MONGOLAB_URI']
   #config.storage = :grid_fs
 
-  #config.grid_fs_access_url = "/tmp"
+  
 
   config.grid_fs_connection = Mongoid.database
   config.storage = :grid_fs
-  config.root = File.join( Rails.root, "tmp" )
+  #config.root = File.join( Rails.root, "tmp" )
+  config.grid_fs_access_url = "/tmp"
 end
