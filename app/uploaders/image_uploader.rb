063c38eab6ee40d0a72df1f19b59209121ebed33
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-12 15:38:38 -0500

    blank defaults

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 	include CarrierWave::RMagick
-  include CarrierWave::Round
 
   storage :grid_fs
 
 	version :thumb do
     process :resize_to_fill => [50, 50]
-    process :rounded_corner
   end
 
   def store_dir
