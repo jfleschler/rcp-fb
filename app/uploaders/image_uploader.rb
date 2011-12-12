857cc3cd48527b453b8469fbff0f571d64c7532d
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-12 12:07:18 -0500

    test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
 	include CarrierWave::RMagick
   include CarrierWave::Round
-  
+
   storage :grid_fs
 
 	version :thumb do
     process :resize_to_fill => [50, 50]
-    process :rounded_corner => [25]
+    process :rounded_corner
   end
 
   def store_dir
@@ -16,6 +16,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def cache_dir
 		"#{Rails.root}/tmp/uploads"
 	end
-
-  
 end
