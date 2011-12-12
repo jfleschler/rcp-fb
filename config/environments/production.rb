de35eeb09b7d405274f1647164e3cd87930cd06f
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-12 10:22:19 -0500

    carrierwav

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ RcpFb::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.middleware.insert_after Rack::Runtime, Rack::GridFS, :prefix => 'tmp', :lookup => :path, :database => ENV['MONGOLAB_URI']
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,5 @@ CarrierWave.configure do |config|
   config.grid_fs_connection = ENV['MONGOLAB_URI']
   config.storage = :grid_fs
 
-  config.grid_fs_access_url = "/images"
+  config.grid_fs_access_url = "/tmp"
 end
