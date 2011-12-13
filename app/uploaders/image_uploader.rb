d0aa2a06a08b6dda1f8cff60e0afa3bc2444d6f2
Jon Fleschler <jon.fleschler@gmail.com>
2011-12-13 11:36:01 -0500

    resize to fill

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,7 +10,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :preview do
-    process :resize_to_fit => [120, 120]
+    process :resize_to_fill => [120, 120]
   end
 
   def store_dir
