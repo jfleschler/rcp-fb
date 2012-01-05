bcc30a20abfdbf307ac16d24e259e748d356b034
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-05 16:21:30 -0500

    testing more

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,3 @@
-APP_CONFIG = APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "app_config.yml"))['production']
+APP_CONFIG = APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "app_config.yml"))[Rails.env]
 
 RECIPE_CATEGORIES = APP_CONFIG['categories'][1]
