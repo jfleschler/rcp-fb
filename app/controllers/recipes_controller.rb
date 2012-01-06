11e9ced38e1eb3e154670178c6daf37aa27f034d
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-06 15:04:57 -0500

    again

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.public #.paginate(:page => params[:page], :per_page => 30)
     if params[:category]
-      @recipes = @recipes.where(category: "Desserts").all
+      @recipes = Recipe.where(category: "#{params[:category]}").public
     end
   end
 
