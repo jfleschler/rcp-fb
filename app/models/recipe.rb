66797b35636d7c716896e3bd6e2aabb9ec024296
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-06 14:19:40 -0500

    try again

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,8 +19,8 @@ class Recipe
 	mount_uploader :image, ImageUploader
 
   default_scope order_by([:created_at, :asc])
-	scope :public, lamda { where(:isPublic => true) }
-	scope :in_category, ->(cate) { where(category: "#{cate}")}
+	scope :public, -> { where(:isPublic => true) }
+	scope :in_category, ->(cate) { where(category: "#{cate}") }
 
 	def step_attributes=(step_attributes)
 	  step_attributes.each do |attributes|
