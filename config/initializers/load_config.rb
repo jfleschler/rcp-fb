10eff8a9fe9461f4af74bc3f0329fe2a1887ec23
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-09 10:00:26 -0500

    replace category with in_category everywhere

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,8 +1,3 @@
 APP_CONFIG = APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "app_config.yml"))[Rails.env]
 
 RECIPE_CATEGORIES = APP_CONFIG['categories']
-
-Recipe.all.each do |r|
-	r.in_category = r.category
-	r.save
-end
