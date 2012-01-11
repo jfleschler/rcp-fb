c4bcefd5adbae7991e1a8aa414729a023da6db65
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-10 19:14:28 -0500

    my_category = "uncategorized"

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,8 @@
 APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "app_config.yml"))[Rails.env]
 
 RECIPE_CATEGORIES = APP_CONFIG['categories']
+
+Recipe.all.each do |r|
+  r.my_category = "Uncategorized"
+  r.save
+end
