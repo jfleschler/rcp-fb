f08cddda99b33c1ce1b4a3d60989154af3d89547
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-11 13:05:32 -0500

    return documents instead of criteria

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,9 +4,9 @@ class RecipesController < ApplicationController
 
   def index
     if params[:c]
-      @recipes = Recipe.public.in_c(params[:c].strip)
+      @recipes = Recipe.public.in_c(params[:c].strip).documents
     elsif params[:t]
-      @recipes = Recipe.public.in_t(params[:t].strip)
+      @recipes = Recipe.public.in_t(params[:t].strip).documents
     else
       @recipes = Recipe.public #.paginate(:page => params[:page], :per_page => 30)
     end
