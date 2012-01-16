a77450dc84799206796dbf688ef15c401203b64d
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-16 09:47:33 -0500

    removed offeset for scroller bottom

--- app/assets/javascripts/scroller.js
+++ app/assets/javascripts/scroller.js
@@ -35,7 +35,7 @@ MYAPP.FixedInfoScroller = function (options) {
 
 			// Get the current scroll of the window.
 			var viewTop = view.scrollTop() + 80;
-			var fixedElementBottom = fixedElement.offset().top + fixedElement.height() + 3;
+			var fixedElementBottom = fixedElement.offset().top + fixedElement.height();
 
 			// Get the current left offset for FixedElement
 			var fixedElementLeft = (($(document).width() - 940) / 2) + 20
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,10 +49,10 @@ module SessionsHelper
 
 	private
 		def store_location
-	      session[:return_to] = request.fullpath
-	    end
+      session[:return_to] = request.fullpath
+    end
 
-	    def clear_return_to
-	      session[:return_to] = nil
-	    end
+    def clear_return_to
+      session[:return_to] = nil
+    end
 end
