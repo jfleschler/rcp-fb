044a1c5c694b3aee71d75ac4ba82189589327e84
Jon Fleschler <jon.fleschler@gmail.com>
2012-01-20 14:33:19 -0500

    testing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -154,9 +154,9 @@ class RecipesController < ApplicationController
   end
 
   def search
+    @user = nil
     if params[:u]
-      uid = params[:u].strip
-      @user2 = User.find(uid)
+      @user = User.find(params[:u])
       # if @user == current_user
       #   if params[:c] && params[:c] != "all" && params[:c] != ""
       #     @recipes = Recipe.u_search(uid, params[:search].strip).in_c(params[:c].strip) #.paginate(:page => params[:page], :per_page => 30)
@@ -171,7 +171,7 @@ class RecipesController < ApplicationController
       #     @recipes = Recipe.u_search(uid, params[:search].strip).public
       #   end
       # end
-      @recipes = Recipe.u_search(uid,params[:search].strip).public
+      @recipes = @user.recipes.search(params[:search].strip).public
     else
       if params[:c] && params[:c] != "all" && params[:c] != ""
         @recipes = Recipe.search(params[:search].strip).in_c(params[:c].strip).public
